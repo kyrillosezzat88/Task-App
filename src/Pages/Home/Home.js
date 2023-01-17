54ae69ccf10f618260c9c172404a1ccda79038f2
ezzat <ezzat@mauve.de>
2023-01-17 11:23:49 +0200

    test

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -7,24 +7,28 @@ import TaskCard from "../../Components/TaskCard/TaskCard";
 import { loadData } from "../../Context/Actions/Auth-Actions";
 import { GetAllUserTasks } from "../../Context/Actions/Task-Actions";
 import { useTask } from "../../Context/TaskContext";
-import Spinner from '../../Assets/Images/spinner.gif'
+import Spinner from "../../Assets/Images/spinner.gif";
 import "./Home.scss";
 function Home() {
   const TasksCollection = collection(db, "tasks");
   const { TaskApp, dispatch } = useTask();
   useEffect(() => {
-    if(!TaskApp.Tasks.length){
-      dispatch(loadData(true));
-      (async () => {
-        if(TaskApp.user.uid){
-          let q = query(TasksCollection,where('userID','==',TaskApp.user.uid));
-          let userTasks = await getDocs(q).then(res => res.docs.map(doc => ({...doc.data() , id:doc.id})))
+    if (!TaskApp.Tasks.length) {
+      dispatch(loadData(true))(async () => {
+        if (TaskApp.user.uid) {
+          let q = query(
+            TasksCollection,
+            where("userID", "==", TaskApp.user.uid)
+          );
+          let userTasks = await getDocs(q).then((res) =>
+            res.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+          );
           dispatch(GetAllUserTasks(userTasks));
           dispatch(loadData(false));
         }
       })();
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [TaskApp.user.uid]);
   return (
     <>
@@ -35,37 +39,46 @@ function Home() {
         </div>
       ) : (
         <section className="Home">
-        <FilterBar />
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-4">
-          {TaskApp.Tasks && TaskApp.Tasks.length > 0 && (
-            TaskApp.Tasks.map(
-              ({
-                title,
-                description,
-                status,
-                dueDate,
-                createdAt,
-                priority,
-                id
-              }) => (
-                <TaskCard
-                  key={id}
-                  title={title}
-                  description={description}
-                  status={status}
-                  dueDate={ dueDate.seconds ? new Date(dueDate.seconds * 1000).toDateString() : new Date(dueDate).toDateString()  }
-                  createdAt={new Date(createdAt.seconds * 1000).toDateString()}
-                  priority={priority}
-                  id={id}
-                />
-              )
-            )
+          <FilterBar />
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-4">
+            {TaskApp.Tasks &&
+              TaskApp.Tasks.length > 0 &&
+              TaskApp.Tasks.map(
+                ({
+                  title,
+                  description,
+                  status,
+                  dueDate,
+                  createdAt,
+                  priority,
+                  id,
+                }) => (
+                  <TaskCard
+                    key={id}
+                    title={title}
+                    description={description}
+                    status={status}
+                    dueDate={
+                      dueDate.seconds
+                        ? new Date(dueDate.seconds * 1000).toDateString()
+                        : new Date(dueDate).toDateString()
+                    }
+                    createdAt={new Date(
+                      createdAt.seconds * 1000
+                    ).toDateString()}
+                    priority={priority}
+                    id={id}
+                  />
+                )
+              )}
+          </div>
+          {!TaskApp.Tasks.length && (
+            <h2 className="text-secondary font-bold text-center mt-10 text-3xl ">
+              No Tasks Created!!{" "}
+            </h2>
           )}
-        </div>
-        {!TaskApp.Tasks.length&&<h2 className="text-secondary font-bold text-center mt-10 text-3xl ">No Tasks Created!! </h2>}
-      </section>
+        </section>
       )}
-
     </>
   );
 }
