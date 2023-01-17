6febc301a88aa42d1928918fd16ee781d664179c
ezzat <ezzat@mauve.de>
2023-01-17 11:19:14 +0200

    test

--- src/Context/ViewPortContext.js
+++ src/Context/ViewPortContext.js
@@ -1,25 +1,27 @@
-// this context to detect screen size to show specific components based on screen size 
+// this context to detect screen size to show specific components based on screen size
 
-import { createContext , useContext, useEffect , useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const Viewportcontext = createContext({
-    isMobile:window.innerWidth  < 769
-})
+  isMobile: window.innerWidth < 769,
+});
 
-export const ViewportProvider = ({children}) => {
-    const [isMobile , setMobile] = useState(window.innerWidth < 769);
-       //handle resize event 
-    useEffect(() => {
-        window.addEventListener('resize', () => setMobile(window.outerWidth < 769));
-        return window.removeEventListener('resize', () => setMobile(window.outerWidth < 769));
-    }, [])
-    return(
-        <Viewportcontext.Provider  value={{ isMobile }}>
-            {children}
-        </Viewportcontext.Provider>
-    )
-}
+export const ViewportProvider = ({ children }) => {
+  const [isMobile, setMobile] = useState(window.innerWidth < 769);
+  //handle resize event
+  useEffect(() => {
+    window.addEventListener("resize", () => setMobile(window.outerWidth < 769));
+    return window.removeEventListener("resize", () =>
+      setMobile(window.outerWidth < 769)
+    );
+  }, []);
+  return (
+    <Viewportcontext.Provider value={{ isMobile }}>
+      {children}
+    </Viewportcontext.Provider>
+  );
+};
 
-export function useViewport(){
-    return useContext(Viewportcontext)
+export function useViewport() {
+  return useContext(Viewportcontext);
 }
